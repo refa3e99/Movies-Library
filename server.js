4c8011df9e1941e647bbe4eb5c6e8a2d30f22efb
refa3e99 <refa3e99@icloud.com>
2022-05-07 23:26:53 +0300

    task12

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require("axios").default;
 
 const moviesData = require("./Movie Data/data.json")
 
@@ -10,8 +11,51 @@ app.get("/",homePage);
 
 app.get("/favorite",favoritePage);
 
+app.get("/trending",trendingHandler);
+
+app.get("/search", searchMovieHandler);
+
+function searchMovieHandler(req , res){
+   // console.log(req.query);
+    //res.send("searching")
+
+    let movieName = req.query.name;
+    let url = "https://api.themoviedb.org/3/search/movie?api_key=668baa4bb128a32b82fe0c15b21dd699&language=en-US&query=The&page=2"
+    axios.get(url)
+    .then(result => {
+        console.log(page.results)
+    })
+    .catch()
+}
+
 function favoritePage(req , res) {
     res.send("Welcome to Favorite Page");
+}
+
+function trendingHandler(req,res){
+    let url = "https://api.themoviedb.org/3/trending/all/week?api_key=37ddc7081e348bf246a42f3be2b3dfd0&language=en-US"
+    axios.get(url)
+    .then(result => {
+        console.log(result.data)
+        res.send("api gave me data")
+
+       let movies = result.page.results.map((movie) => {
+           return new Movie(
+               movie.id,
+               movie.title,
+               movie.release_date,
+               movie.poster_path,
+               movie.overview
+            );
+       })
+       res.json(movies);  
+    }
+        
+    )
+    .catch(error => {
+        console.log(error);
+        res.send("error in getting data from the API")
+    })
 }
 
 function homePage(req , res){
@@ -32,8 +76,10 @@ function homePage(req , res){
 }
 
 //constructor
-function Movie(title , poster_path , overview){
+function Movie(id , title , release_date , poster_path , overview){
+    this.id = id;
     this.title = title;
+    this.release_date = release_date;
     this.poster_path = poster_path;
     this.overview = overview
 }
@@ -54,3 +100,5 @@ app.use((req, res, next) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
