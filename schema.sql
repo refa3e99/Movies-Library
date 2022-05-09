50796c157bcafdc5f7d41e3e1afc50bed49d6276
refa3e99 <refa3e99@icloud.com>
2022-05-10 00:16:48 +0300

    task14 solved

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS movies;
 
-CREATE TABLE movies (
+CREATE TABLE IF NOT EXISTS movies (
+    id SERIAL PRIMARY KEY,
     title varchar(255),
     category varchar(255),
     overview varchar(255),
